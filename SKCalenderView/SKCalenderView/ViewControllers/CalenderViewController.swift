c0f3255c28c77566d645a247b3cb0f177398841d
subrat_kumar_kheti <subrat_kumar_kheti@dell.com>
2020-09-27 11:35:58 +0530

    Initial Commit

---
+++ SKCalenderView/SKCalenderView/ViewControllers/CalenderViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SKCalenderView
+//
+//  Created by Subrat Kheti on 27/09/20.
+//  Copyright © 2020 Subrat Kheti. All rights reserved.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SKCalenderView/SKCalenderView/Views/CalenderBannerView.swift
@@ -0,0 +1,49 @@
+//
+//  CalenderBannerView.swift
+//  SKCalenderView
+//
+//  Created by Subrat Kheti on 27/09/20.
+//  Copyright © 2020 Subrat Kheti. All rights reserved.
+//
+
+import UIKit
+
+class CalenderBannerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+   
+    let nibName = "CalenderBannerView"
+    //var contentView:UIView?
+    
+    @IBOutlet weak var label: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        //contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+
+}
